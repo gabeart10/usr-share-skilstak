d8ab1126c4777de80ae2327daffcab816b321dd6
Rob Muhlestein <rob@skilstak.com>
2015-09-30 14:10:14 -0400

    save

--- motd/motd.py
+++ motd/motd.py
@@ -8,7 +8,7 @@ euid =  os.geteuid()
 name =  pwd.getpwuid(euid)[4].split(',')[0]
 groups = os.getgroups()
 #isuser = os.popen('/usr/local/bin/name ' + name).read() 
-bux = os.popen('/usr/share/skilstak/bin/bux').read() 
+#bux = os.popen('/usr/share/skilstak/bin/bux').read() 
 
 #if not isuser: exit()
 
@@ -16,7 +16,7 @@ print(c.yellow + "Welcome back " + name + ". Good to see you." + c.reset)
 #print(c.red + "Fall registration and payment due before August 1st" + c.reset)
 #if 9000 in groups:
 #    print(c.yellow + "I see you are enrolled as a student." + c.reset)
-print(bux, end='')
+#print(bux, end='')
 #else:
 #    print(c.red + 'Looks like you are no longer enrolled, your login is limited.' + c.reset)
 #    print(c.red + 'You are free to code and talk and behave but not keep anything running.' + c.reset)

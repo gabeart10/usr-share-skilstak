4ae4268230bef0b972562b8f28a5c9d22d95f9d3
Rob Muhlestein <rob@skilstak.com>
2015-10-26 20:06:46 -0400

    save

--- lib/python/skilstak/colors.py
+++ lib/python/skilstak/colors.py
@@ -101,8 +101,20 @@ def random_color():
 rc = random_color
 random = random_color
 
-'''(Deprecated) Clears the screen'''
+def multi(text):
+    """Prints text with each character a random color.
+    
+    Args:
+        text (str): The text to print.
+
+    """
+    buf = ''
+    for char in text:
+        buf += rc() + char + x
+    return buf
+
 def clear_screen():
+    """(Deprecated) Clears the screen"""
     print(clear,end='')
 cs = clear_screen
 clears = clear_screen
@@ -134,3 +146,7 @@ if __name__ == '__main__':
     for count in range(7):
         print(random_color() + 'Random' + reset, end=' ')
     print()
+
+    print()
+
+    print(multi("multicolor"))
